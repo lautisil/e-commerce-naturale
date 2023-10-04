6b09e0dad21cce95c151537f07bfc2d1bac4d2b8
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2023-10-04 16:28:16 -0300

    chakra y menu

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import CartWidget from './CartWidget'
-
+import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
 
 const NavBar = () => {
   return (
     <div>
         <h1>NaturAle</h1>  {/* Brand */}
+        <Menu>
+          <MenuButton>
+            Menu
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Ofertas Semanales</MenuItem>
+            <MenuItem>Productos</MenuItem>
+            <MenuItem>Proximos en venta</MenuItem>
+            <MenuItem>Carrito</MenuItem>
+            <MenuItem>Perfil</MenuItem>
+          </MenuList>
+        </Menu>
         <CartWidget /> {/* Carrito */}
     </div>
     
