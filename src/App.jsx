60d1876b29811de6be204586f51a6238ddd8d53a
Jorge Lautaro Silvestro <lautarosilvestro2000@hotmail.com>
2023-10-04 17:05:53 -0300

    after

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Bienvenida from './componentes/Bienvenida'
 import NavBar from './componentes/NavBar'
 import ItemListContainer from './componentes/ItemListContainer'
 
--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
 import CartWidget from './CartWidget'
-import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
+import { Menu, MenuButton, MenuList, MenuItem, Flex, Box, Spacer } from '@chakra-ui/react'
 
 const NavBar = () => {
   return (
     <div>
-        <h1>NaturAle</h1>  {/* Brand */}
-        <Menu>
-          <MenuButton>
-            Menu
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Ofertas Semanales</MenuItem>
-            <MenuItem>Productos</MenuItem>
-            <MenuItem>Proximos en venta</MenuItem>
-            <MenuItem>Carrito</MenuItem>
-            <MenuItem>Perfil</MenuItem>
-          </MenuList>
-        </Menu>
-        <CartWidget /> {/* Carrito */}
+
+          <Flex>
+            <Box p='4' bg='red.400'>
+              <h1>NaturAle</h1>  {/* Brand */}
+            </Box>
+            <Spacer />
+
+            <Box p='4' bg='blue.400'>
+              <Menu>
+                <MenuButton>
+                  Menu
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Ofertas Semanales</MenuItem>
+                  <MenuItem>Productos</MenuItem>
+                  <MenuItem>Proximos en venta</MenuItem>
+                  <MenuItem>Carrito</MenuItem>
+                  <MenuItem>Perfil</MenuItem>
+                </MenuList>
+              </Menu>
+            </Box>
+            <Spacer />
+
+            <Box p='4' bg='green.400'>
+              <CartWidget /> {/* Carrito */}
+            </Box>
+          </Flex>
+        
     </div>
     
   )
